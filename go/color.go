db79cdccd9c2c4fc61f10fa3c7d457eebf42c6d4
Matt Pillar <matt@aerofs.com>
2017-01-30 15:20:36 -0800

    Complete the Go chapter

--- go/color.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/fatih/color"
-
-func main() {
-	color.Red("Hello, world!")
-}
---
+++ go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+type Channel struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+	router := NewRouter()
+
+	router.Handle("channel add", addChannel)
+	//router.Handle("channel subscribe", subscribeChannel)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
+}
